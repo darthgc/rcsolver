4646aed0d15722cfee0c2859f0b6fd536ee069f9
Guillaume Croteau <guillaume.croteau.1@gmail.com>
2017-04-23 17:45:43 -0400

    Added 'Hello World' NXT program.

---
+++ src/com/gctse/rcsolver/RCsolver.java
@@ -0,0 +1,12 @@
+package com.gctse.rcsolver;
+
+import lejos.nxt.Button;
+
+public class RCsolver {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+        Button.waitForAnyPress();
+	}
+
+}
